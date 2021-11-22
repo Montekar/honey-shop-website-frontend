346da09fd775fd4a32905817843b674d54a64967
Adam Ondrejkovic <adam.ondrejkovic@gmail.com>
2021-11-22 17:53:13 +0100

    Finished header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
+import { LoginDropdownComponent } from './header/login-dropdown/login-dropdown.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginDropdownComponent
   ],
   imports: [
     BrowserModule,
