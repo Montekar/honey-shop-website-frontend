187fd97746c747a772fc070387e7a9a5bf32900e
Adam Ondrejkovic <adam.ondrejkovic@gmail.com>
2021-12-10 10:46:00 +0100

    Shopping cart setup

---
+++ src/app/_models/shoppingCartItem.ts
@@ -0,0 +1,7 @@
+import { Product } from './product'
+
+export class ShoppingCartItem {
+  id!: number;
+  product!: Product;
+  amount!: number;
+}
--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from '../_services/shopping-cart.service'
+import { first } from 'rxjs/operators'
+import { ShoppingCartItem } from '../_models/shoppingCartItem'
 
 @Component({
   selector: 'app-shopping-cart',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-cart.component.scss']
 })
 export class ShoppingCartComponent implements OnInit {
+  allItems: any;
 
-  constructor() { }
+  constructor(private shoppingCartService: ShoppingCartService) { }
 
   ngOnInit(): void {
+    this.shoppingCartService.getAll()
+      .pipe(first())
+      .subscribe(cartItems => this.allItems = cartItems)
+  }
+
+  deleteItem(id: number){
+    this.shoppingCartService.deleteItem(id)
+  }
+
+  updateItem(item: ShoppingCartItem){
+    this.shoppingCartService.updateItem(item)
   }
 
 }
